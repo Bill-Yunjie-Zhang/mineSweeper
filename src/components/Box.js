69000856eb4ae1fb495a50a9039bf475cd07eaec
Bill <207559339@qq.com>
2020-03-05 21:10:45 +0800

    colors

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,18 +1,26 @@
 import React from "react"
 
-const style = {
-    height: 30,
-    width: 30,
-    fontSize: 20,
-    verticalAlign: "top"
-}
+// let style = {
+//     height: 30,
+//     width: 30,
+//     fontSize: 20,
+//     verticalAlign: "top",
+//     color: "black"
+// }
 
 class Box extends React.Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            value: ""
+            value: "",
+            style: {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "black"
+            }
         }
     }
     setId = (num) => {
@@ -22,8 +30,89 @@ class Box extends React.Component {
     }
 
     render() {
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 0){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#bdbdbd"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 1){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#4646ff"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 2){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#099c09"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 3){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#ff4444"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 4){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#01016b"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 5){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#770000"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 6){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#012201"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 7){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#160122"
+            }
+        }
+        if((this.props.squareTaken)[this.setId(this.props.id)[0]][this.setId(this.props.id)[1]] === 8){
+            this.state.style = {
+                height: 30,
+                width: 30,
+                fontSize: 20,
+                verticalAlign: "top",
+                color: "#000000"
+            }
+        }
         return (
-            <button class={"Buttons"} style={style} id={this.props.id} onContextMenu={() => {
+            <button class={"Buttons"} style={this.state.style} id={this.props.id} onContextMenu={() => {
                 this.props.open(this.setId(this.props.id), true)
                 this.props.resetNum()
                 this.props.forceUpdateHandler()
